823104b65bfeb8ae42377e6d50b8319abe628c10
dsfgdsfgsdfg456456 <humairahmed.allfree@gmail.com>
2024-10-13 04:13:03 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -16,10 +16,10 @@ module.exports = async (req, res) => {
     try {
         const geoRes = await fetch(geoUrl);
         const geoData = await geoRes.json();
-        const userCountry = geoData.countryCode || "default";
+        const userCountry = geoData.countryCode || "US";
         
         // Set the URL to redirect based on country
-        const redirectUrl = urlList[userCountry] || urlList["default"];
+        const redirectUrl = urlList[userCountry] || urlList["US"];
 
         // Set Chrome user-agent
         res.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36");
@@ -29,7 +29,7 @@ module.exports = async (req, res) => {
         res.end();
     } catch (error) {
         console.error("Error fetching geo data:", error);
-        res.writeHead(302, { Location: urlList["default"] });
+        res.writeHead(302, { Location: urlList["US"] });
         res.end();
     }
 };
